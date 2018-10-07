977f8891156467793ec9c7eec24b469d53103a20
Juuso Lehtinen <jupelehtinen@gmail.com>
2018-10-07 20:10:16 +0300

    Fix person events rendering, if no known events

--- src/components/PersonEvents/index.js
+++ src/components/PersonEvents/index.js
@@ -7,19 +7,32 @@ import { renderAge } from '../../util';
 import styles from './personEvents.css';
 
 const renderDate = date => date && (date.length === 4 ? date : format(date, 'DD.MM.YYYY'));
+const renderAgeOnEvent = (birth, event) => {
+  if (!birth) {
+    return null;
+  }
 
-export default ({ events, birth }) => (
-  <table className={styles.table}>
-    <tbody>
-      {events.map(event => (
-        <tr key={event.id}>
-          <td className={styles.date}>{renderDate(event.date)}</td>
-          <td className={styles.age}>{event.type !== 'Birth' && renderAge(birth, event.date)}</td>
-          <td><Translate id={`events.${event.type}`} /></td>
-          <td>{event.description}</td>
-          <td className={styles.place}>{event.place && event.place.name}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-);
+  return event.type !== 'Birth' && renderAge(birth, event.date);
+};
+
+export default ({ events, birth }) => {
+  if (!events) {
+    return null;
+  }
+
+  return (
+    <table className={styles.table}>
+      <tbody>
+        {events.map(event => (
+          <tr key={event.id}>
+            <td className={styles.date}>{renderDate(event.date)}</td>
+            <td className={styles.age}>{renderAgeOnEvent(birth, event)}</td>
+            <td><Translate id={`events.${event.type}`} /></td>
+            <td>{event.description}</td>
+            <td className={styles.place}>{event.place && event.place.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
