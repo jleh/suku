a4b0d57b4d6673cf0b2d321f0559a2cde24ec915
Juuso Lehtinen <jupelehtinen@gmail.com>
2019-05-26 23:19:06 +0300

    Use hooks

--- src/components/Person/index.js
+++ src/components/Person/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Translate } from 'react-localize-redux';
@@ -16,66 +16,58 @@ import PersonMap from '../PersonMap';
 import CoatOfArms from './CoatOfArms';
 import ProfilePicture from './ProfilePicture';
 
-class Person extends Component {
-  componentDidMount() {
-    window.scrollTo(0, 0);
+const Person = ({
+  personsById, personsByRef, placesById, match
+}) => {
+  const person = personsById.get(match.params.id);
+
+  if (!person || !person.name) {
+    return null;
   }
 
-  render() {
-    const {
-      personsById, personsByRef, placesById, match
-    } = this.props;
+  const { personEvents, birthISO } = person.events;
 
-    const person = personsById.get(match.params.id);
+  useEffect(() => window.scrollTo(0, 0));
 
-    if (!person || !person.name) {
-      return null;
-    }
-
-    const { personEvents, birthISO } = person.events;
-
-    return (
-      (
-        <div className={styles.person}>
-          <div className={styles.header}>
-            <ProfilePicture person={person} />
-            <div className={styles.info}>
-              <div>
-                <h2>{person.name}</h2>
-                <PersonDates events={person.events} />
-                <Wikipedia person={person} />
-              </div>
-              <CoatOfArms coatOfArms={person.coatOfArms} />
-            </div>
+  return (
+    <div className={styles.person}>
+      <div className={styles.header}>
+        <ProfilePicture person={person} />
+        <div className={styles.info}>
+          <div>
+            <h2>{person.name}</h2>
+            <PersonDates events={person.events} />
+            <Wikipedia person={person} />
           </div>
-
-          <div className={styles.content}>
-            <div className={styles.personEvents}>
-              <h3><Translate id="person.life" /></h3>
-              <PersonEvents events={personEvents} birth={birthISO} places={placesById} />
-            </div>
-
-            <div>
-              <h3><Translate id="person.family" /></h3>
-              <div className={styles.parents}>
-                <Translate id="person.parents" />:
-                <LinkedPerson personRef={person.father} persons={personsByRef} />
-                <LinkedPerson personRef={person.mother} persons={personsByRef} />
-              </div>
-
-              <Family families={person.family} persons={personsByRef} />
-            </div>
-          </div>
-
-          <PersonMap events={person.events.personEvents} places={placesById} />
-          <Sources sources={person.sources} />
-
-          <NavigationButtons />
+          <CoatOfArms coatOfArms={person.coatOfArms} />
         </div>
-      )
-    );
-  }
-}
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.personEvents}>
+          <h3><Translate id="person.life" /></h3>
+          <PersonEvents events={personEvents} birth={birthISO} places={placesById} />
+        </div>
+
+        <div>
+          <h3><Translate id="person.family" /></h3>
+          <div className={styles.parents}>
+            <Translate id="person.parents" />:
+            <LinkedPerson personRef={person.father} persons={personsByRef} />
+            <LinkedPerson personRef={person.mother} persons={personsByRef} />
+          </div>
+
+          <Family families={person.family} persons={personsByRef} />
+        </div>
+      </div>
+
+      <PersonMap events={person.events.personEvents} places={placesById} />
+      <Sources sources={person.sources} />
+
+      <NavigationButtons />
+    </div>
+  );
+};
 
 const mapStateToProps = ({ persons, places }) => ({
   personsById: persons.personsById,
