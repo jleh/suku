8cea2ac4832fd61af0873bb19fef86ed51cf5852
Juuso Lehtinen <juuso.lehtinen@ohjelmalehtinen.fi>
2022-11-14 17:58:33 +0200

    Show more places on map

--- src/components/PersonMap/index.js
+++ src/components/PersonMap/index.js
@@ -6,22 +6,37 @@ import uniq from 'lodash/uniq';
 import { getCoordinates } from '../../util';
 
 import styles from './personMap.css';
+import { useSelector } from 'react-redux';
 
 const PersonMap = ({ events, places }) => {
+  const { placesById } = useSelector((state) => state.places);
+
   if (!events) {
     return null;
   }
 
   const eventPlaces = uniq(
-    events
-      .map((event) => places.get(event.place.id))
-      .filter((place) => place !== undefined)
-      .filter((place) => place.lat !== null)
+    events.map((event) => places.get(event.place.id)).filter((place) => place !== undefined)
   );
 
   if (eventPlaces.length === 0) {
     return null;
   }
+
+  // Find some coordinates for places without those
+  const getCoordinatesFromParent = (place) => {
+    const parent = placesById.get(place.parent);
+    if (parent.lat === null && parent.parent) return getCoordinatesFromParent(parent);
+    else return { lat: parent.lat, lng: parent.lng };
+  };
+
+  eventPlaces.forEach((place) => {
+    if (place.lat === null) {
+      const { lat, lng } = getCoordinatesFromParent(place);
+      place.lat = lat;
+      place.lng = lng;
+    }
+  });
 
   let bounds;
   let center;
@@ -42,11 +57,13 @@ const PersonMap = ({ events, places }) => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        {eventPlaces.map((place) => (
-          <Marker key={place.id} position={getCoordinates(place)}>
-            <Popup>{place.name}</Popup>
-          </Marker>
-        ))}
+        {eventPlaces
+          .filter((place) => place.lng)
+          .map((place) => (
+            <Marker key={place.id} position={getCoordinates(place)}>
+              <Popup>{place.name}</Popup>
+            </Marker>
+          ))}
       </Map>
     </div>
   );
